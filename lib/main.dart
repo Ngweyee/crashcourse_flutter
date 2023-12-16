24c0d560bf69e9fe67112589b07d0e1ad5a77478
Ngwe_Yee_Shoon <lamin.ngweyeeshoon@gmail.com>
2023-12-16 23:02:33 +0000

    -lesson 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,9 @@
+import 'location_detail.dart';
 import 'package:flutter/material.dart';
 
 void main() {
    runApp(MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(title: const Text("Hello Eli!")),
-       body: Column(
-         mainAxisAlignment: MainAxisAlignment.start,
-         crossAxisAlignment: CrossAxisAlignment.stretch,
-         children: [
-           Container(
-             decoration: const BoxDecoration(
-                 color: Colors.red
-             ),
-             child: const Text("One"),
-           ),
-           Container(
-             decoration: const BoxDecoration(
-                 color: Colors.green
-             ),
-             child: const Text("Two"),
-           ),Container(
-             decoration: const BoxDecoration(
-                 color: Colors.blue
-             ),
-             child: const Text("Three"),
-           )
-         ],
-       )
-     ),
+     home: LocationDetail()
      ),
    );
 }
