c0b59812983604374b290e4e0368ffab5be7217f
Ngwe_Yee_Shoon <lamin.ngweyeeshoon@gmail.com>
2023-12-17 20:14:05 +0000

    -lesson 11

--- lib/models/location_fact.dart
+++ lib/models/location_fact.dart
@@ -1,8 +1,11 @@
+import 'package:json_annotation/json_annotation.dart';
 
+@JsonSerializable()
 class LocationFact {
-
   final String title;
   final String text;
   LocationFact({required this.title, required this.text});
 
+  // factory LocationFact.fromJson(Map<String, dynamic> json) =>
+  //     _$LocationFactFromJson(json);
 }
---
+++ test/unit/location_test.dart
@@ -0,0 +1,7 @@
+import 'package:crashcourse_flutter/models/location.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:crashcourse_flutter/mocks/mock_location.dart';
+
+void main(){
+
+}
