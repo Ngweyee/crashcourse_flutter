986b436612a58f7cba0c96621f2ab77daa6b5310
Ngwe_Yee_Shoon <lamin.ngweyeeshoon@gmail.com>
2023-12-17 03:49:56 +0000

    -lesson 5

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'models/location.dart';
 
-class LocationDetail extends StatelessWidget{
+class LocationDetail extends StatelessWidget {
   final Location location;
 
   LocationDetail(this.location);
@@ -13,26 +13,50 @@ class LocationDetail extends StatelessWidget{
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _renderFacts(location),
+          children: _renderBody(context, location),
         )
     );
   }
 
-  List<Widget> _renderFacts(Location location){
+  List<Widget> _renderFacts(Location location) {
     List<Widget> result = [];
-    for(int i = 0; i< location.facts.length; i++){
+    for (int i = 0; i < location.facts.length; i++) {
       result.add(_sectionTitle(location.facts[i].title));
       result.add(_sectionText(location.facts[i].text));
     }
     return result;
   }
 
+  List<Widget> _renderBody(BuildContext context, Location location) {
+    List<Widget> result = [];
+    result.add(_bannerImage(location.url, 170.0));
+    result.addAll(_renderFacts(location));
+    return result;
+  }
+
+
   Widget _sectionText(String text) {
-    return Text(text);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
+      child: Text(text)
+      );
   }
 
   Widget _sectionTitle(String title) {
-    return Text(title);
+    return Container(
+      padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+      child: Text(title, textAlign: TextAlign.left, style: TextStyle(
+        fontSize: 25.0,
+        color: Colors.black,
+        ),
+      ),
+    );
   }
 
+  Widget _bannerImage(String imageUrl, double height) {
+    return Container(
+      constraints: BoxConstraints.tightFor(height: height),
+      child: Image.network(imageUrl, fit: BoxFit.fitWidth),
+    );
+  }
 }
--- lib/mocks/mock_location.dart
+++ lib/mocks/mock_location.dart
@@ -7,7 +7,9 @@ class MockLocation extends Location{
 
 
   static Location FetchAny(){
-    return Location(name: "Arashiyama Bamboo Grove, Kyoto, Japan", url: "https://goo.gl/Npy2K8", facts: <LocationFact> [
+    return Location(name: "Arashiyama Bamboo Grove",
+        url: "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg",
+        facts: <LocationFact> [
       LocationFact(title: "Summary", text: "While we could go on about the ethernal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight's pleasures extend beyond the pleasure realm."),
       LocationFact(title: "How to Get There", text: "Kyoto airport, with several terminal, is located 16 kilometers south of the city and is also known as Kyoto, Kyoto can also be reached by transport links from other regional airports")
     ]);
